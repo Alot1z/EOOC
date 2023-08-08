ba102f2c86e5397cf7046a0d92116485d99c6f44
everything <16801672+Alot1z@users.noreply.github.com>
2023-08-08 23:47:11 +0200

    f

--- advanced-scripts.js
+++
@@ -1,9 +0,0 @@
-// Avanceret JavaScript-funktionalitet
-document.addEventListener("DOMContentLoaded", function() {
-  // Din avancerede JavaScript-funktionalitet her
-  const parallaxContent = document.querySelector(".parallax-content");
-  window.addEventListener("scroll", function() {
-    const yOffset = window.pageYOffset;
-    parallaxContent.style.transform = `translateY(-${yOffset * 0.5}px)`;
-  });
-});
